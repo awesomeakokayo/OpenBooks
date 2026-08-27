04bc0f4d8625c503cd6946d8eba0136d261cf326
Awesome Stephens <awesomeakokayo@gmail.com>
2026-08-27 23:52:15 +0100

    ui: strengthen shared card language

--- components/ui/Card.tsx
+++ components/ui/Card.tsx
@@ -1,18 +1,13 @@
 import * as React from "react";
 
 export function Card({ className = "", ...props }: React.HTMLAttributes<HTMLDivElement>) {
-  return (
-    <div
-      className={`rounded-[16px] bg-white border border-plum/10 shadow-[0_4px_20px_rgba(80,48,71,0.06)] ${className}`}
-      {...props}
-    />
-  );
+  return <div className={`openbooks-card ${className}`} {...props} />;
 }
 
 export function CardHeader({ className = "", ...props }: React.HTMLAttributes<HTMLDivElement>) {
-  return <div className={`p-5 pb-3 ${className}`} {...props} />;
+  return <div className={`p-6 pb-3 ${className}`} {...props} />;
 }
 
 export function CardContent({ className = "", ...props }: React.HTMLAttributes<HTMLDivElement>) {
-  return <div className={`p-5 pt-0 ${className}`} {...props} />;
+  return <div className={`p-6 pt-0 ${className}`} {...props} />;
 }
