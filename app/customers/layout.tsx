bcefc5cbeca3cf03688fb6ddec9840022fb49452
Awesome Stephens <awesomeakokayo@gmail.com>
2026-08-29 09:32:55 +0100

    refactor: add workspace shell to customer routes

---
+++ app/customers/layout.tsx
@@ -0,0 +1,5 @@
+import { WorkspaceShell } from "@/components/workspace/WorkspaceShell";
+
+export default function CustomersLayout({ children }: { children: React.ReactNode }) {
+  return <WorkspaceShell>{children}</WorkspaceShell>;
+}
